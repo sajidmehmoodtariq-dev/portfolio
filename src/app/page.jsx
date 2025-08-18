4149644622bf106ce4a728aa2b005d9c122a259b
sajidmehmoodtariq30 <sajidmehmoodtariq5@gmail.com>
2025-08-19 00:05:57 +0500

    Tape Section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Header from "@/sections/Header"
 import Hero from "@/sections/Hero"
 import Projects from "@/sections/Projects"
+import Tape from "@/sections/Tape"
 
 
 const page = () => {
@@ -9,6 +10,7 @@ const page = () => {
       <Header />
       <Hero />
       <Projects />
+      <Tape />
     </>
   )
 }
