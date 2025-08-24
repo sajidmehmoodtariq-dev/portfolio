93c66cffab6ac566b182e71e9de39dead2fefe22
sajidmehmoodtariq30 <sajidmehmoodtariq5@gmail.com>
2025-08-24 22:06:04 +0500

    Metadata for blogs

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -1,3 +1,44 @@
+import { cookies } from 'next/headers';
+
+// Dynamic metadata for SEO
+export async function generateMetadata({ params }) {
+  // SSR fetch for the blog post
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs/${params.slug}`, {
+    headers: { Cookie: cookies().toString() },
+    cache: 'no-store',
+  });
+  if (!res.ok) {
+    return {
+      title: 'Blog Not Found | Sajid Mehmood Tariq',
+      description: 'This blog post could not be found.'
+    };
+  }
+  const data = await res.json();
+  const blog = data?.data?.blog;
+  if (!blog) {
+    return {
+      title: 'Blog Not Found | Sajid Mehmood Tariq',
+      description: 'This blog post could not be found.'
+    };
+  }
+  return {
+    title: `${blog.title} | Sajid Mehmood Tariq`,
+    description: blog.excerpt || blog.title,
+    openGraph: {
+      title: `${blog.title} | Sajid Mehmood Tariq`,
+      description: blog.excerpt || blog.title,
+      images: blog.imageUrl ? [blog.imageUrl] : ['/logo.png'],
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blogs/${blog.slug}`,
+      type: 'article',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: `${blog.title} | Sajid Mehmood Tariq`,
+      description: blog.excerpt || blog.title,
+      images: blog.imageUrl ? [blog.imageUrl] : ['/logo.png'],
+    },
+  };
+}
 "use client";
 
 import { useState, useEffect } from 'react';
